ce40a42f98544aafbde68264ecf8bd31b66c0519
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-03-26 18:32:18 -0500

    [Feature] Second exercise, logic ready

--- 0x00/src/App.js
+++ 0x00/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Test1 from './components/Test1';
+import Test2 from './components/Test2';
 
 function App() {
   return (
     <>
-      <Test1 />
+      <Test2 />
     </>
   );
 }
--- 0x00/src/components/Test2.jsx
+++ 0x00/src/components/Test2.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Aside from './Aside';
 
 const Test2 = () => {
+  const [value, setValue] = useState('');
+  const [message, setMessage] = useState('');
+
+const handleChange = (e) => setValue(e.target.value);
+
+const valueIsValid = () => {
+  if (value === '') {
+    setMessage('Error: Por favor ingrese un número entero igual o mayor a 1');
+    return false;
+  } else if (isNaN(value)) {
+    setMessage('Error: esto no es un número entero');
+    return false;
+  } else if (value.includes('.')) {
+    setMessage('Error: dede ingresar un número entero, no se acepta número flotante');
+    return false;
+  } else if (value < 1) {
+    setMessage('Error: el número debe ser igual o mayor a 1');
+    return false;
+  } else {
+      return true;
+  }
+}
+
+const handleSubmit = (e)=> {
+  e.preventDefault();
+  if (valueIsValid()) {
+    const num = parseInt(value);
+    let sequenceAkelab = [];
+    for (let i = 1; i <= num; i++) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        if (i % 3 === 0 && i % 5 === 0) {
+          sequenceAkelab.push('AKELAB');
+        } else if (i % 3 === 0) {
+          sequenceAkelab.push('AKE');
+        } else {
+          sequenceAkelab.push('LAB');
+        }
+      } else {
+        sequenceAkelab.push(i);
+      }
+    }
+    setMessage(sequenceAkelab.join(', '))
+  }
+}
+
   return (
-    <main role='main'>
-      in test2...
-    </main>
+    <>
+      <main role='main'>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="number">Ingrese un número entero igual o mayor a 1</label>
+          <input type="text" name="number" id="number" placeholder='6' onChange={handleChange}/>
+          <button type="submit">mostrar secuencia</button>
+        </form>
+      </main>
+      <Aside message={message}/>
+    </>
   )
 }
 
-export default Test2
+export default Test2;
