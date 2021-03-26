36f099975276c3c0bcfcff2076d220c98886fab7
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-03-26 14:22:20 -0500

    [Feature] Working in test1

--- 0x00/src/App.js
+++ 0x00/src/App.js
@@ -1,22 +1,8 @@
 import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './components/Home';
 import Test1 from './components/Test1';
-import Test2 from './components/Test2';
-import Test3 from './components/Test3';
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Home />
-      <Test1 />
-      <Test2 />
-      <Test3 />
-      <Footer />
-    </>
-  );
+  return <Test1 />;
 }
 
 export default App;
--- 0x00/src/components/Test1.jsx
+++ 0x00/src/components/Test1.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Test1 = () => {
+  const [value, setValue] = useState('');
+  const [message, setMessage] = useState('');
+
+const handleChange = (e)=>{
+            setValue(e.target.value)
+}
+
+const valueIsValid = () => {
+  if (value === '') {
+    console.log('Error: Por favor ingrese un número entero igual o mayor a 1');
+    return false;
+  } else if (isNaN(value)) {
+    console.log('Error: esto no es un número entero');
+    return false;
+  } else if (value.includes('.')) {
+    console.log('Error: dede ingresar un número entero, no se acepta número flotante');
+    return false;
+  } else if (value < 1) {
+    console.log('Error: el número debe ser igual o mayor a 1');
+    return false;
+  } else {
+      return true;
+  }
+}
+
+const handleSubmit = (e)=> {
+  e.preventDefault();
+  if (valueIsValid()) {
+    const num = parseInt(value);
+    console.log('continuamos....', num)
+  } else {
+    console.log(error);
+  }
+}
+
   return (
     <main role='main'>
-      in test1...
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="number">Ingrese un número entero igual o mayor a 1</label>
+        <input type="text" name="number" id="number" placeholder='6' onChange={handleChange}/>
+        <button type="submit">Serie Fibonacci</button>
+      </form>
     </main>
   )
 }
