06565525bdc720af986b116cddec890b1fceb400
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-03-27 15:05:40 -0500

    [Feature] Server created, GET, POST methods tested

---
+++ 0x00-server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const morgan = require('morgan');
+const movies = require('./Movies.json');
+const allow = require('./Allow.json');
+
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.all('/movies', (req, res, next) => {
+  if (
+    req.method === 'GET' &&
+    req.url === `/movies?${allow.user}=${allow.password}`
+  ) {
+    next();
+  } else if (
+    req.method === 'POST' &&
+    req.url === '/movies' &&
+    req.body.user === allow.user &&
+    req.body.password === allow.password
+  ) {
+    next();
+  } else {
+    res.json({ denied: true, message: 'Acceso denegado' });
+  }
+});
+
+app.get('/movies', (req, res) => {
+  res.json(movies);
+});
+
+app.post('/movies', (req, res) => {
+  res.json(movies);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
